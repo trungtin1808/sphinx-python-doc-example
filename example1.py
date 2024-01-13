fb7adca5f49b89a0d08348f88ef682933de043be
trungtin1808 <trungtintran1808@gmail.com>
2024-01-13 13:31:51 +0700

    adding functions to document

---
+++ example1.py
@@ -0,0 +1,16 @@
+def func1(arg1, arg2):
+    """
+    This function take two arguments, sets the first to equal the second, the return the new first argument. Pointless.
+    :param arg1: Some value
+    :param arg2: Another value
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
+
+def func2():
+    """
+
+    :return: None
+    """
+    return None
